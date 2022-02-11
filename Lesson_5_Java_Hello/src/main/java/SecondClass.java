f7a0b1379ac40596c87695786b5515707f1a5c76
YashchicheC <98526529+YashchicheC@users.noreply.github.com>
2022-02-11 20:33:14 +0200

    правки по форматированию строки и вынесение gitignore в корень

---
+++ Lesson_5_Java_Hello/src/main/java/SecondClass.java
@@ -0,0 +1,9 @@
+public class SecondClass
+{
+    public void methodPrint()
+    {
+        int i = 11111;
+        String str = String.format("---<%s>---", i);
+        System.out.printf(str);
+    }
+}
