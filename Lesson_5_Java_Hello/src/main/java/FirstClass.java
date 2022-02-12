d977722537f5e44bceec177f8a668b0ad88eba30
YashchicheC <98526529+YashchicheC@users.noreply.github.com>
2022-02-12 08:50:10 +0200

    adding duplicate print save return to a new variable

--- Lesson_5_Java_Hello/src/main/java/FirstClass.java
+++ Lesson_5_Java_Hello/src/main/java/FirstClass.java
@@ -3,6 +3,9 @@ public class FirstClass
     public static void main(String[] args)
     {
         SecondClass sc = new SecondClass();
-        sc.methodPrint();
+     //   sc.methodPrint();
+        String strFC;
+        strFC = sc.methodPrint();
+        System.out.printf(strFC);
     }
 }
