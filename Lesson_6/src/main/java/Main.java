8edb79037383fb08c5b74ccede712a975d45fded
YashchicheC <98526529+YashchicheC@users.noreply.github.com>
2022-02-16 07:32:16 +0200

    вынесение отдельно переприсваивания значения

--- Lesson_6/src/main/java/Main.java
+++ Lesson_6/src/main/java/Main.java
@@ -1,5 +1,7 @@
-public class Main {
-    public static void main(String[] args) {
+public class Main
+{
+    public static void main(String[] args)
+    {
         int i1 = 1;
         int i2 = 3;
         int i3 = 2;
@@ -9,8 +11,10 @@ public class Main {
         int result2 = i3 + i4;
 
         System.out.println("result1 > result2 = " + (result1 > result2));
-        System.out.println("result1+1 = " + (result1 + 1));
-        System.out.println("result2-2 = " + (result2 - 2));
+        result1 += 1;
+        result2 = result2 - 2;
+        System.out.println("result1+1 = " + result1);
+        System.out.println("result2-2 = " + result2);
         System.out.println("result1 > result2 second comparison = " + (result1 > result2));
         System.out.println("result1 or result2 are multiple of 2 = " + (result1%2 ==0 || result2%2 ==0));
     }
